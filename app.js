8a7bf562e92a0e71173163ed8c1e1834cdf61922
pixelandpage <mags@pixelandpage.co.uk>
2018-01-15 22:52:50 +0000

    posts a kudos message to your chosen channel

--- app.js
+++ app.js
@@ -1,34 +1,47 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const { WebClient } = require('@slack/client');
 
 const app = express();
+
+const authToken = process.env.SLACK_AUTH_TOKEN;
+const channelId = process.env.CHANNEL_ID;;
 
 app.use(bodyParser.urlencoded());
 
 const parseMessage = (message) => {
-    const regex = /(\@\w*)(.*)/;
+    const regex = /(\@\w*)([\s\S]*)/;
     const chunks = message.text.match(regex)
     const target = chunks[1];
     const kudosString = chunks[2];
     return {target, kudosString};
 };
 
-const echo = (target, kudosString) => {
-    return {
-        "text": `Kudos <${target}> :tada:`,
-        "attachments": [
-            {
-                "text":kudosString
-            }
-        ]
-    }
-}
+const sendKudos = (client, target, kudosString) => {
+    return client.chat.postMessage(`${channelId}`, `Hey <${target}>, someone has sent you kudos :tada:`, {
+        attachments: [{
+            text:kudosString
+        }]
+    })
+};
 
 app.get('/kudos', (req, res) => {res.send('KUDOS')})
 
 app.post('/kudos', (req, res) => {
     const {target, kudosString} = parseMessage(req.body)
-    res.json(echo(target, kudosString));
+    const client = new WebClient(authToken);
+
+    sendKudos(client, target, kudosString)
+    .then(() => res.json({
+        text: `You send this message of appreciation to <${target}>`,
+        attachments: [{
+            text:`${kudosString}`
+        }]
+    }))
+    .catch((err) => res.json({text: `Oops, something went wrong: ${err}`}))
+
 });
 
 app.listen(3456);
